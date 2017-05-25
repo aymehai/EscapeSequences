ca9391dbc288b5d5f7883256208fc689568d46bb
student <student@GBTC440J11LX.montgomerycollege.edu>
2017-05-25 14:54:07 -0400

    Escape Sequence

---
+++ src/EscapeSequences.java
@@ -0,0 +1,30 @@
+
+public class EscapeSequences {
+
+	public static void main(String[] args) {
+
+		System.out.println("*********");
+		System.out.println("*\t*");
+		System.out.println("*\t*");
+		System.out.println("*\t*");
+		System.out.println("*\t*");
+		System.out.println("*********");
+		
+		System.out.println(" ");
+		
+		System.out.println("  ***");
+		System.out.println(" *   *");
+		System.out.println("*     *");
+		System.out.println("*     *");
+		System.out.println(" *   *");
+		System.out.println("  ***");
+		
+		System.out.println(" ");
+		
+		System.out.println("   *\n  * *\n *   *\n *   *\n  * *\n   *");
+		
+		
+		
+	}
+
+}
